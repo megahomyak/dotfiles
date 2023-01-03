d5270893e933da599dc651c52b3654c5f71f41bf
megahomyak <g.megahomyak@gmail.com>
2023-01-03 22:39:59 +0500

    Added a `userChrome.css` for Firefox

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 link() {
-    rm ~/$1/$2 2> /dev/null
-    mkdir -p ~/$1
-    ln -rs $1/$2 ~/$1/$2
+    rm ~/"$1/$2"
+    mkdir -p ~/"$1"
+    ln -rs "$1/$2" ~/"$1/$2"
 }
 
 link .config/nvim init.vim
@@ -9,3 +9,10 @@ link .cargo config.toml
 link . .bash_utils
 link .termux termux.properties
 link . .inputrc
+
+FIREFOX_PATH="$(python get_firefox_default_profile_path.py)"
+FIREFOX_CHROME_PROFILE_FILE_NAME='userChrome.css'
+FIREFOX_CHROME_PATH="$FIREFOX_PATH/chrome"
+rm "$FIREFOX_CHROME_PATH"/"$FIREFOX_CHROME_PROFILE_FILE_NAME"
+mkdir -p "$FIREFOX_CHROME_PATH"
+ln -rs "firefox/$FIREFOX_CHROME_PROFILE_FILE_NAME" "$FIREFOX_CHROME_PATH/$FIREFOX_CHROME_PROFILE_FILE_NAME"
