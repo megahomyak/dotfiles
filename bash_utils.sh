a59fb8ec0738889289a33881557f99f30f3a12aa
megahomyak <g.megahomyak@gmail.com>
2024-02-17 11:45:40 +0500

    Removed them colors because they don't seem to work properly

--- bash_utils.sh
+++ bash_utils.sh
@@ -110,7 +110,8 @@ export PS1="$(
     RESET="\[\e[0m\]"
     GREEN="\[\e[1;32m\]"
     BLUE="\[\e[1;34m\]"
-    echo "$GRAY\D{%d.%m.%Y} \t$RESET $GREEN\u@\h$RESET:$BLUE\w$RESET\$ "
+    echo "\D{%d.%m.%Y} \t \u@\h:\w\$ "
+    #echo "$GRAY\D{%d.%m.%Y} \t$RESET $GREEN\u@\h$RESET:$BLUE\w$RESET\$ "
 )"
 
 switch() {
