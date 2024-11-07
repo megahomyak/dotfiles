f4273f957711ae9079850f588f002b4c9d044229
megahomyak <g.megahomyak@gmail.com>
2024-11-07 20:35:41 +0500

    Removed xsession because it doesn't help with launching CopyQ

--- apply.sh
+++ apply.sh
@@ -18,5 +18,4 @@ link fonts/JetBrainsMono-Bold.ttf ~/.local/share/fonts/JetBrainsMono-Bold.ttf
 link fonts/JetBrainsMono-SemiBold.ttf ~/.local/share/fonts/JetBrainsMono-SemiBold.ttf
 link flameshot.ini ~/.config/flameshot/flameshot.ini
 link copyq.conf ~/.config/copyq/copyq.conf
-link .xsession ~/.xsession
 link xinput/90-darn-tablet.conf /usr/share/X11/xorg.conf.d/90-darn-tablet.conf
