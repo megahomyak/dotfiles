b951b19612da41c4db9b14166cbbe397b664a9be
megahomyak <g.megahomyak@gmail.com>
2024-12-04 00:01:26 +0500

    Better "firefox_use_xinput2.sh"

--- ON_DEMAND/firefox_use_xinput2.sh
+++ ON_DEMAND/firefox_use_xinput2.sh
@@ -1,1 +1,13 @@
-sed -i 's/^Exec=/Exec=env MOZ_USE_XINPUT2=1 /' /usr/share/applications/firefox.desktop
+read -p "This is really fucking dangerous. Do you really wish to proceed? (y/n): " yn
+
+if [ "$yn" == "y" ]; then
+sudo bash << EOF
+mv /usr/lib/firefox/firefox-bin /usr/lib/firefox/true-firefox-bin
+cat > /usr/lib/firefox/firefox-bin << EOF2
+#!/bin/bash
+export MOZ_USE_XINPUT2=1
+exec /usr/lib/firefox/true-firefox-bin "\\\$@"
+EOF2
+chmod +x /usr/lib/firefox/firefox-bin
+EOF
+fi
