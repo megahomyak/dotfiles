794346c255c8deecc35792c6ccc3f7f1ce151942
megahomyak <g.megahomyak@gmail.com>
2025-10-27 23:53:14 +0500

    Added a file to install some much needed stuff

---
+++ ON_DEMAND/install_everything_pc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install copyq flameshot
+echo 'Install Firefox through ./install_firefox_on_ubuntu.sh, install Telegram from website, install Nekobox from Github releases, install neovim from Github releases'
