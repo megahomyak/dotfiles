2aa3205e9fbe64646974a68b8887508c0c0adae9
megahomyak <g.megahomyak@gmail.com>
2025-10-30 10:40:14 +0500

    "apply" now switches between sudo/nosudo when necessary

--- apply.sh
+++ apply.sh
@@ -1,33 +1,27 @@
-if [ "$NOSUDO" == "" ]; then
-    link() {
-        sudo rm "$2"
-        sudo mkdir -p "$(dirname "$2")"
-        sudo ln -rs "$1" "$2"
-    }
-else
-    link() {
-        rm "$2"
-        mkdir -p "$(dirname "$2")"
-        ln -rs "$1" "$2"
-    }
-fi
+nosudo() { "$@"; }
 
-link init.vim ~/.config/nvim/init.vim
-link hotfuzz_control_panel/commands ~/.config/hotfuzz_control_panel/commands
-link cargo/config.toml ~/.cargo/config.toml
-link bash_utils.sh ~/bash_utils.sh
-link termux.properties ~/.termux/termux.properties
-link .inputrc ~/.inputrc
-link .gitconfig ~/.gitconfig
-link kitty.conf ~/.config/kitty/kitty.conf
-link journald.conf /etc/systemd/journald.conf
-link .Xresources ~/.Xresources
-link fonts/JetBrainsMono-Bold.ttf ~/.local/share/fonts/JetBrainsMono-Bold.ttf
-link fonts/JetBrainsMono-SemiBold.ttf ~/.local/share/fonts/JetBrainsMono-SemiBold.ttf
-link fonts/JetBrainsMono-Bold.ttf ~/.termux/font.ttf
-link flameshot.ini ~/.config/flameshot/flameshot.ini
-link copyq.conf ~/.config/copyq/copyq.conf
-link xinput/90-darn-tablet.conf /usr/share/X11/xorg.conf.d/90-darn-tablet.conf
-link hwdb.d/99-capslk-esc-swap.hwdb /etc/udev/hwdb.d/99-capslk-esc-swap.hwdb
-link .vimrc ~/.vimrc
-link security/limits.conf /etc/security/limits.conf
+link() {
+    "$1" rm "$3"
+    "$1" mkdir -p "$(dirname "$3")"
+    "$1" ln -rs "$2" "$3"
+}
+
+link nosudo init.vim ~/.config/nvim/init.vim
+link nosudo hotfuzz_control_panel/commands ~/.config/hotfuzz_control_panel/commands
+link nosudo cargo/config.toml ~/.cargo/config.toml
+link nosudo bash_utils.sh ~/bash_utils.sh
+link nosudo termux.properties ~/.termux/termux.properties
+link nosudo .inputrc ~/.inputrc
+link nosudo .gitconfig ~/.gitconfig
+link nosudo kitty.conf ~/.config/kitty/kitty.conf
+link sudo journald.conf /etc/systemd/journald.conf
+link nosudo .Xresources ~/.Xresources
+link nosudo fonts/JetBrainsMono-Bold.ttf ~/.local/share/fonts/JetBrainsMono-Bold.ttf
+link nosudo fonts/JetBrainsMono-SemiBold.ttf ~/.local/share/fonts/JetBrainsMono-SemiBold.ttf
+link nosudo fonts/JetBrainsMono-Bold.ttf ~/.termux/font.ttf
+link nosudo flameshot.ini ~/.config/flameshot/flameshot.ini
+link nosudo copyq.conf ~/.config/copyq/copyq.conf
+link sudo xinput/90-darn-tablet.conf /usr/share/X11/xorg.conf.d/90-darn-tablet.conf
+link sudo hwdb.d/99-capslk-esc-swap.hwdb /etc/udev/hwdb.d/99-capslk-esc-swap.hwdb
+link nosudo .vimrc ~/.vimrc
+link sudo security/limits.conf /etc/security/limits.conf
