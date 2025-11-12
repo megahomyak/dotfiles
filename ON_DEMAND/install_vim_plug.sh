6f6130ddd5fa854068a1d58fd477cb4e98c84a1a
megahomyak <g.megahomyak@gmail.com>
2025-11-12 17:50:12 +0500

    Software updates, +ON_DEMAND/install_vim_plug.sh, but most importantly, +"git-rm-local" && "git-view-rm-local"

---
+++ ON_DEMAND/install_vim_plug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+sudo apt install curl
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim -c :PlugInstall
