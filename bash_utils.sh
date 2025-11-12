21e3ce47e97a0c0c86dcda864cbee92983f42c4c
megahomyak <g.megahomyak@gmail.com>
2025-11-12 17:59:01 +0500

    Removed git-*-rm-local as they don't seem to work

--- bash_utils.sh
+++ bash_utils.sh
@@ -218,11 +218,3 @@ export secret_stash_connect_timeout=5
 alias ss=~/i/secret_stash/secret_stash.sh
 
 export PAGER=less
-
-git-rm-local() {
-    git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done
-}
-
-git-view-rm-local() {
-    git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do printf '%s' "$branch"; done
-}
